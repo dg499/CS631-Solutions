c75eec12504013026111e90366f42b276a9c7d37
dg499 <47801685+dg499@users.noreply.github.com>
2019-02-24 22:48:40 -0500

    seventh query answer

--- NJIT databse.sql
+++ NJIT databse.sql
@@ -15,25 +15,33 @@ select * from LINEITEM;
 7. List the products never ordered by customers of New Jersey
 */
 
-select distinct c.name from customer c,"ORDER" o,product p,lineitem l where c.CID=o.CID and o.OID=l.OID and l.PID=p.PID and p.ProductName='Computer';
+SELECT DISTINCT C.NAME FROM CUSTOMER C,"ORDER" O,PRODUCT P,LINEITEM L WHERE C.CID=O.CID AND O.OID=L.OID AND L.PID=P.PID AND P.PRODUCTNAME='Computer';
 
-select distinct p.productname from product p ,lineitem l,"ORDER" o,customer c where c.CID in
-(select c.CID from customer c,"ORDER" o,product p,lineitem l where c.CID=o.CID and o.OID=l.OID and l.PID=p.PID and p.ProductName='Computer')and c.CID=o.CID and 
-p.PID=l.PId and l.OID=o.OID and p.productname!='computer';
+SELECT DISTINCT P.PRODUCTNAME FROM PRODUCT P ,LINEITEM L,"ORDER" O,CUSTOMER C WHERE C.CID IN
+(SELECT C.CID FROM CUSTOMER C,"ORDER" O,PRODUCT P,LINEITEM L WHERE C.CID=O.CID AND O.OID=L.OID AND L.PID=P.PID AND P.PRODUCTNAME='Computer')AND C.CID=O.CID AND 
+P.PID=L.PID AND L.OID=O.OID AND P.PRODUCTNAME!='Computer';
 
-select c.CID,c.name from customer c,"ORDER" o where c.CID=o.CID and o."DATE" < TO_DATE('01-Jan-2014','DD-MON-YYYY'); 
+SELECT C.CID,C.NAME FROM CUSTOMER C,"ORDER" O WHERE C.CID=O.CID AND O."DATE" < TO_DATE('01-Jan-2014','DD-MON-YYYY'); 
 
-select p.productname from product p,"ORDER" o,lineitem l,customer c where p.PID=l.PID and l.OID=o.OID and o.CID=c.CID  and c.city='Newark';
+SELECT DISTINCT P.PRODUCTNAME FROM PRODUCT P,"ORDER" O,LINEITEM L,CUSTOMER C WHERE P.PID=L.PID AND L.OID=O.OID AND O.CID=C.CID  AND C.CITY='Newark';
+
+
+
 
 --NOT(products that are not bought by at least one customer of Newark)
-select distinct l.PID, p.productname from lineitem l,product p where p.PID=l.PID and l.PID NOT IN
-(select l1.PID from "ORDER" o,customer c,lineitem l1 where o.OID=l1.OID and c.CID=o.CID  and c.city='Newark' and l1.PID NOT IN
-(select l2.PID from lineitem l2 where l2.OID=l1.OID));
+SELECT DISTINCT L.pid, P.productname FROM lineitem L,product P WHERE P.pid=L.pid AND L.pid NOT IN
+(SELECT l1.pid FROM "ORDER" o,customer C,lineitem l1 WHERE o.OID=l1.OID AND C.cid=o.cid  AND C.city='Newark' AND l1.pid NOT IN
+(SELECT l2.pid FROM lineitem l2 WHERE l2.OID=l1.OID));
 
 --NOT(products ordered by at least 1 customer not from Newark)
 select p.PID,p.productname from product p where p.PID NOT IN
 (select distinct p1.PID from product p1,"ORDER" o,lineitem l,customer c where p1.PID=l.PID and l.OID=o.OID and o.CID=c.CID  and c.city<>'Newark');
 select p.productname from product p minus select p.productname from product p,"ORDER" o,lineitem l,customer c where p.PID=l.PID and l.OID=o.OID and o.CID=c.CID  and c.city!='Newark';
+
+
+SELECT DISTINCT  P.productname FROM product P,lineitem L WHERE P.pid=L.pid  AND  L.pid NOT IN
+(SELECT DISTINCT L.PID  FROM LINEITEM L, "ORDER" O,PRODUCT P, CUSTOMER C WHERE 
+L.PID=P.PID AND  L.OID=O.OID AND O.CID=C.CID  AND  C.state='NJ' ) ;
 
 
 drop view productmonth;
@@ -52,9 +60,6 @@ drop view mostpopularproduct;
 create view mostpopularproduct as
 select pid,productname,numberofcustomer from
 (select p.pid, p.productname ,  count(distinct c.cid) as numberofcustomer  from customer c,"ORDER" o,product p,lineitem l where c.CID=o.CID and o.OID=l.OID and l.PID=p.PID group by p.pid, p.productname order by count(distinct c.cid) desc ) fetch first 1 rows only;
-
-
-
 
 select * from mostpopularproduct;
 
